95a4a1dc177113dc7167d5b98667737e4a7c5ae9
Jona <jona.oestreicher@gmail.com>
2024-10-09 09:07:16 +0200

    updated test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 from experiment_handling.experiment import Experiment
 from experiment_handling.enivronment_loader import EnvironmentLoader
 from experiment_handling.io import IOConfiguration
-from experiment_handling.wandb.manager import WandbManager, WandbConfiguration
+from experiment_handling.wandb.manager import WandbConfiguration, WandbManager
 from experiment_handling.config_loader import ConfigLoader
 from dataclasses import dataclass
 
@@ -29,14 +29,20 @@ class Config:
     training_parameters: TrainingParameters
 
 
+def training_process(training_params: TrainingParameters):
+    pass
+
+
 def main():
-    config: Config = ConfigLoader(
-        "experiment_handling/config.yaml"
-    ).get_config_as_dataclass(dataclass=Config)
+    config = ConfigLoader("experiment_handling/config.yaml").get_config_as_dataclass(
+        dataclass=Config
+    )
     print(config)
 
     wandb_manager = WandbManager(config)
     wandb_manager.inititialize_wandb()
+
+    training_process(config.training_parameters)
 
     print(config.io.output_dir)
 
